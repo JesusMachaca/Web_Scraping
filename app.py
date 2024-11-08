da2600e7f73ddd058d6803dea57ac327e6f06667
JesusMachaca <106131017+JesusMachaca@users.noreply.github.com>
2024-11-08 13:52:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
         cursor = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
         
         # Consulta SQL para recuperar datos de la tabla Ofertas
-        query = "SELECT titulo, empresa, ubicacion, requerimientos, enlace FROM ofertas_laborales"
+        query = "SELECT titulo, empresa, ubicacion, requerimientos, enlace FROM ofertas_laborales where lower(titulo) like '%pre%'"
         cursor.execute(query)
         
         # Almacenar resultados en una lista de diccionarios
